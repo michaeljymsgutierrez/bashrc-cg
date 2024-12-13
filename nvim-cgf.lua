a02b3ea8bcc528fb3114fe7286bfca4efacf641f
michaeljymsgutierrez <leeez711@gmail.com>
2024-12-13 17:56:16 +0800

    Added monokai-pro for nvim

--- nvim-cgf.lua
+++ nvim-cgf.lua
@@ -1,43 +1,44 @@
 -- SETUP
 -- Copy this lines to ~/.config/nvim/init.lua
--- local homeDir = os.getenv("HOME") .. "/bashrc-cg/nvim-cgf.lua"
+-- local homeDir = os.getenv('HOME") .. "/bashrc-cg/nvim-cgf.lua"
 -- local initNvimConfig = loadfile(homeDir)
 -- initNvimConfig()
 
 -- INITIAL CONFIG
-vim.cmd("set number")
-vim.cmd("set number")
-vim.cmd("set nocompatible")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set expandtab")
-vim.cmd("set showmatch")
-vim.cmd("set t_Co=256")
-vim.cmd("set laststatus=2")
-vim.cmd("set hlsearch")
-vim.cmd("set incsearch")
-vim.cmd("set ignorecase")
-vim.cmd("set smartcase")
-vim.cmd("set wildignore+=*.swp")
-vim.cmd("set mouse=a")
-vim.cmd("set clipboard+=unnamed")
-vim.cmd("set timeoutlen=1000")
-vim.cmd("set ttimeoutlen=0")
-vim.cmd("set backspace=indent,eol,start")
-vim.cmd("set noshowmode")
+vim.cmd('set number')
+vim.cmd('set number')
+vim.cmd('set nocompatible')
+vim.cmd('set tabstop=2')
+vim.cmd('set shiftwidth=2')
+vim.cmd('set expandtab')
+vim.cmd('set showmatch')
+vim.cmd('set t_Co=256')
+vim.cmd('set laststatus=2')
+vim.cmd('set hlsearch')
+vim.cmd('set incsearch')
+vim.cmd('set ignorecase')
+vim.cmd('set smartcase')
+vim.cmd('set wildignore+=*.swp')
+vim.cmd('set mouse=a')
+vim.cmd('set clipboard+=unnamed')
+vim.cmd('set timeoutlen=1000')
+vim.cmd('set ttimeoutlen=0')
+vim.cmd('set backspace=indent,eol,start')
+vim.cmd('set noshowmode')
+vim.cmd('set background=dark')
 
 -- PLUGIN MANAGER
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
 
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
 
     vim.fn.getchar()
@@ -49,16 +50,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- LAZY PLUGINS
-require("lazy").setup({
+require('lazy').setup({
   spec = {
-    -- add your plugins here
+    { 'loctvl842/monokai-pro.nvim' }
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
+  install = { colorscheme = { 'monokai-pro' } },
   checker = { enabled = true },
 })
+
+vim.cmd('colorscheme monokai-pro')
 
 -- KEYBOARD MAPPING CONFIG
 vim.api.nvim_set_keymap('n', '<C-Left>', ':tabprevious<CR>', { noremap = true })
