d0cf32c7de804684ed0a0a5a00f8154379622272
michaeljymsgutierrez <leeez711@gmail.com>
2024-12-13 16:39:55 +0800

    Updated nvim lua file

--- nvim-cgf.lua
+++ nvim-cgf.lua
@@ -1,8 +1,10 @@
+-- SETUP
 -- Copy this lines to ~/.config/nvim/init.lua
 -- local homeDir = os.getenv("HOME") .. "/bashrc-cg/nvim-cgf.lua"
 -- local initNvimConfig = loadfile(homeDir)
 -- initNvimConfig()
 
+-- INITIAL CONFIG
 vim.cmd("set number")
 vim.cmd("set number")
 vim.cmd("set nocompatible")
@@ -24,19 +26,36 @@ vim.cmd("set ttimeoutlen=0")
 vim.cmd("set backspace=indent,eol,start")
 vim.cmd("set noshowmode")
 
+-- PLUGIN MANAGER
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
+
     vim.fn.getchar()
+
     os.exit(1)
   end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
+-- LAZY PLUGINS
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
