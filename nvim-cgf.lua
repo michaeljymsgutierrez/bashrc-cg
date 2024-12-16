d838fcea65831928545ee1dbe0860ea92e29cdb5
michaeljymsgutierrez <leeez711@gmail.com>
2024-12-16 13:57:20 +0800

    Temporary remove markdown preview

--- nvim-cgf.lua
+++ nvim-cgf.lua
@@ -160,15 +160,12 @@ require('lazy').setup({
           textobjects = { enable = true }
         })
       end
-    }
+    },
     -- {
     --   'maxmellon/vim-jsx-pretty',
     --   config = function()
     --     vim.cmd('let g:vim_jsx_pretty_colorful_config = 1')
     --   end
-    -- },
-    -- {
-    --   'iamcco/markdown-preview.nvim'
     -- },
   },
   install = {},
