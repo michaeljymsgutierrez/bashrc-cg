182997bd0fc71638991bd86869dbb18968fcd068
michaeljymsgutierrez <leeez711@gmail.com>
2025-01-03 10:01:41 +0800

    Added overwrite for accepting supermaven key accept suggestions

--- nvim-cgf.lua
+++ nvim-cgf.lua
@@ -73,7 +73,11 @@ require('lazy').setup({
     {
       'supermaven-inc/supermaven-nvim',
       config = function()
-        require('supermaven-nvim').setup({})
+        require('supermaven-nvim').setup({
+          keymaps = {
+            accept_suggestion = '<C-Space>'
+          },
+        })
       end
     },
     {
