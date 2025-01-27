09fc365f7936cd763c0524ecdc1ca382ae70e8ee
michaeljymsgutierrez <leeez711@gmail.com>
2025-01-27 18:52:04 +0800

    Added tab drop function when opening new and existing buffer

--- nvim-cgf.lua
+++ nvim-cgf.lua
@@ -152,7 +152,20 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = {
         'nvim-lua/plenary.nvim'
-      }
+      },
+      config = function()
+        require('telescope').setup {
+          defaults = {
+            mappings = {
+              i = {
+                ['<cr>'] = function(bufnr)
+                  require('telescope.actions.set').edit(bufnr, 'tab drop')
+                end
+              }
+            }
+          }
+        }
+      end
     },
     {
       'nvim-lualine/lualine.nvim',
