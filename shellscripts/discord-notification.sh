6e3c2f792ed37ceeb92f76a86209a8bf07125bd8
michaeljymsgutierrez <leeez711@gmail.com>
2026-01-14 11:12:15 +0800

    Added discord notification counter

---
+++ shellscripts/discord-notification.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+icons[0]="󰬹"
+icons[1]="󰬺"
+icons[2]="󰬻"
+icons[3]="󰬼"
+icons[4]="󰬽"
+icons[5]="󰬾"
+icons[6]="󰬿"
+icons[7]="󰭀"
+icons[8]="󰭁"
+icons[9]="󰭂"
+
+unread_discord_notification=$(osascript <<'EOT'
+    set discordBadge to 0
+    tell application "System Events"
+        tell process "Dock"
+            try
+                -- Target the specific Discord UI element
+                set badgeValue to value of attribute "AXStatusLabel" of UI element "Discord" of list 1
+
+                -- Check if the badge has a value and isn't empty
+                if badgeValue is not missing value and badgeValue is not "" then
+                    set discordBadge to badgeValue as integer
+                end if
+            on error
+                -- Discord isn't in the Dock or Accessibility is blocked
+                set discordBadge to 0
+            end try
+        end tell
+    end tell
+    return discordBadge
+EOT
+)
+
+
+unread_notification=""
+
+if [ $unread_discord_notification -eq 0 ]; then
+  unread_notification="#[fg=#f8f1ff,bg=#222222,bold]"
+fi
+
+if [ $unread_discord_notification -gt 0 ]; then
+  unread_notification="#[fg=#fde466,bg=#222222,bold]"
+fi
+
+for (( i=0; i<${#unread_discord_notification}; i++ )); do
+  notification_value="${unread_discord_notification:$i:1}"
+  unread_notification+=${icons[$notification_value]}
+done
+
+
+echo "$unread_notification"
+
