2cfb63f8ecd4bb9d09213d0f1acce2f29d316d5a
michaeljymsgutierrez <leeez711@gmail.com>
2026-04-29 23:34:24 +0800

    Updated network icon

--- shellscripts/network.sh
+++ shellscripts/network.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "#[fg=#f8f1ff,bg=#222222,bold]󰲝 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
+echo "#[fg=#f8f1ff,bg=#222222,bold]󰾅 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
 
 if ping -c 2 google.com > /dev/null ; then
-  echo "#[fg=#fde466,bg=#222222,bold]󰱔 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
+  echo "#[fg=#fde466,bg=#222222,bold]󰓅 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
 else
-  echo "#[fg=#fa618d,bg=#222222,bold]󰱟 #[fg=#f8f1ff,bg=#222222,bold]󰫱󰫰󰫻"
+  echo "#[fg=#fa618d,bg=#222222,bold]󰾆 #[fg=#f8f1ff,bg=#222222,bold]󰫱󰫰󰫻"
 fi
+
+# echo "#[fg=#f8f1ff,bg=#222222,bold]󰲝 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
+#
+# if ping -c 2 google.com > /dev/null ; then
+#   echo "#[fg=#fde466,bg=#222222,bold]󰱔 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
+# else
+#   echo "#[fg=#fa618d,bg=#222222,bold]󰱟 #[fg=#f8f1ff,bg=#222222,bold]󰫱󰫰󰫻"
+# fi
