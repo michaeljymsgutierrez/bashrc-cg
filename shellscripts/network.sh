00779beedd40d779ed22eb10b2a6f1ff0c524350
michaeljymsgutierrez <leeez711@gmail.com>
2026-05-03 03:22:57 +0800

    Updated network script

--- shellscripts/network.sh
+++ shellscripts/network.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#! /bin/bash
 
-echo "#[fg=#f8f1ff,bg=#222222,bold]󰌚 #[fg=#f8f1ff,bg=#222222,bold]󰫶󰫻"
+connecting_icon=$(shuf -e 󰤟  󰤢  󰤥  -n 1)
+connected_icon=$(shuf -e  󰤡  󰤤  󰤧  -n 1)
+disconnected_icon=$(shuf -e 󰤠  󰤣  󰤦  -n 1)
+
+echo "#[fg=#f8f1ff,bg=#222222,bold]$connecting_icon #[fg=#f8f1ff,bg=#222222,bold]󰫶󰫻"
 
 if ping -c 2 google.com > /dev/null ; then
-  echo "#[fg=#fde466,bg=#222222,bold]󰌘 #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
+  echo "#[fg=#fde466,bg=#222222,bold]$connected_icon #[fg=#f8f1ff,bg=#222222,bold]󰫰󰫻"
 else
-  echo "#[fg=#fa618d,bg=#222222,bold]󰌙 #[fg=#f8f1ff,bg=#222222,bold]󰫱󰫰"
+  echo "#[fg=#fa618d,bg=#222222,bold]$disconnected_icon #[fg=#f8f1ff,bg=#222222,bold]󰫱󰫰"
 fi
